115b4f46535c1c3199adf6c5b53463181196bddc
Yerkebulan-bit <63576399+Yerkebulan-bit@users.noreply.github.com>
2021-05-23 23:30:22 +0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FIS___Final_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OleDbConnection conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=..\\Debug\\Database.accdb");
+            OleDbCommand com = new OleDbCommand();
+            OleDbDataAdapter adap = new OleDbDataAdapter();
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            com.Connection = conn;
+            com.CommandText = "SELECT Book.Name, Book.Genre, Book.Rating, Author.Name " +
+                "FROM Book LEFT JOIN Author ON Author.ID = Book.Author_ID " +
+                "Where Book.Name Like '%" + textBox1.Text + "%'" +
+                "ORDER BY Book.Name";
+            conn.Open();
+            adap.SelectCommand = com;
+            adap.Fill(ds);
+            dt = ds.Tables[0];
+            dataGridView1.DataSource = dt;
+            conn.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OleDbConnection conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=..\\Debug\\Database.accdb"); 
+            OleDbCommand com = new OleDbCommand();
+            OleDbDataAdapter adap = new OleDbDataAdapter(); 
+            DataSet ds = new DataSet(); 
+            DataTable dt = new DataTable();
+            com.Connection = conn; 
+            com.CommandText = "SELECT Book.Name, Book.Genre, Book.Rating, Author.Name " +
+                "FROM Book LEFT JOIN Author ON Author.ID = Book.Author_ID " +
+                "Where Book.Name Like '%" + textBox1.Text + "%'" +
+                "ORDER BY Book.Name";
+            conn.Open();
+            adap.SelectCommand = com; 
+            adap.Fill(ds); 
+            dt = ds.Tables[0]; 
+            dataGridView1.DataSource = dt; 
+            conn.Close();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Form2 newForm = new Form2();
+            newForm.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 newForm = new Form3();
+            newForm.Show();
+        }
+
+    }
+}
+/*
+            SELECT
+	        cities.id as city_id, cities.name as city_name, cities.country_id as city_country_id,
+	        countries.id as country_id, countries.name as country_name
+            FROM cities
+            LEFT JOIN countries ON countries.id=cities.country_id
+
+            SELECT Book.Name, Book.Genre, Book.Rating, Book.Author_ID, Author.ID FROM Book LEFT JOIN Author ON Author.ID = Book.Author_ID
+            Select Name, Genre, Rating from Book INNER JOIN Author ON Book.Author_ID = Book.ID Where Name Like '%" + textBox1.Text + "%'
+             */
---
+++ Form2.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FIS___Final_Project
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            OleDbConnection conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=..\\Debug\\Database.accdb");
+            OleDbCommand com = new OleDbCommand();
+            OleDbDataAdapter adap = new OleDbDataAdapter();
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            com.Connection = conn;
+            com.CommandText = "SELECT ID, Date_of_publication, Requirement, Book_Name FROM Exchange WHERE Book_owner_ID = 1";
+            conn.Open();
+            adap.SelectCommand = com;
+            adap.Fill(ds);
+            dt = ds.Tables[0];
+            dataGridView1.DataSource = dt;
+            conn.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string date = DateTime.UtcNow.ToString("MM-dd-yyyy");
+            OleDbConnection conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=..\\Debug\\Database.accdb");
+            OleDbCommand com = new OleDbCommand();
+            OleDbDataAdapter adap = new OleDbDataAdapter();
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            conn.Open();
+            OleDbCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "INSERT INTO Exchange ([Date_of_publication],[Requirement],[Book_Name],[Book_owner_ID]) " +
+                "VALUES('" + date + "','" + textBox2.Text + "', '" + textBox1.Text + "', '" + 1 + "')";
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            textBox1.Text = "";
+            textBox2.Text = "";
+            MessageBox.Show("An Item has been successfully added");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OleDbConnection conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=..\\Debug\\Database.accdb");
+            OleDbCommand com = new OleDbCommand();
+            OleDbDataAdapter adap = new OleDbDataAdapter();
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            com.Connection = conn;
+            com.CommandText = "SELECT ID, Date_of_publication, Requirement, Book_Name " +
+                "FROM Exchange " +
+                "WHERE Book_owner_ID = 1";
+            conn.Open();
+            adap.SelectCommand = com;
+            adap.Fill(ds);
+            dt = ds.Tables[0];
+            dataGridView1.DataSource = dt;
+            conn.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OleDbConnection conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=..\\Debug\\Database.accdb");
+            OleDbCommand com = new OleDbCommand();
+            OleDbDataAdapter adap = new OleDbDataAdapter();
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            conn.Open();
+            OleDbCommand cmd = conn.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "DELETE FROM Exchange WHERE Book_Name='" + textBox3.Text + "'";
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Record deleted successfully");
+        }
+    }
+}
